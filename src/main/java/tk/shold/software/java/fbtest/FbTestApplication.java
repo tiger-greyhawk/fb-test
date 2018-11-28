efee14ad1a7b18c7e457869aa82d2efa831b570d
Oleg Kov <tiger.greyhawk@gmail.com>
2018-11-28 13:50:54 +0300

    Initial commit

---
+++ src/main/java/tk/shold/software/java/fbtest/FbTestApplication.java
@@ -0,0 +1,32 @@
+package tk.shold.software.java.fbtest;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sdljava.SDLException;
+import sdljava.SDLMain;
+import sdljava.image.SDLImage;
+
+@SpringBootApplication
+@Log
+public class FbTestApplication {
+
+	public static void main(String[] args) {
+		System.setProperty("java.library.path", "c:\\_src\\_java\\fb-test\\src\\main\\sdljava\\lib");
+
+		try {
+			//SDLMain.getSDLVersion();
+			//System.loadLibrary("SDLJava_image");
+			int height = SDLImage.load("c:\\delete\\destDir\\Fest\\4\\4-0.png").getHeight();
+
+			log.info(""+height);
+		}
+		catch (SDLException e)
+		{
+			log.warning(e.getMessage());
+		}
+		SpringApplication.run(FbTestApplication.class, args);
+
+
+	}
+}
---
+++ src/test/java/tk/shold/software/java/fbtest/FbTestApplicationTests.java
@@ -0,0 +1,16 @@
+package tk.shold.software.java.fbtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FbTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
