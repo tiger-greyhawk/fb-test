a3b42e546a61bdec43f49f3f40074ef7f2ea89b2
Oleg Kov <tiger.greyhawk@gmail.com>
2018-12-01 18:25:34 +0300

    clean files

--- src/main/java/tk/shold/software/java/sdljava/bin/TestTextureGL.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-set CLASSPATH=%CD%\lib;%CD%\lib\sdljava.jar;%CLASSPATH%
-set PATH=%CD%\lib;%PATH%
-java -cp %CLASSPATH% -Djava.library.path=%CD%\lib org.gljava.opengl.TextureTest
-cd bin
-pause
--- src/main/java/tk/shold/software/java/sdljava/bin/testmixer.bat
+++
@@ -1,4 +0,0 @@
-cd ..
-set PATH=%CD%\lib;%PATH%
-java -cp .\lib\sdljava.jar -Djava.library.path=.\lib sdljava.mixer.SDLMixerTest
-cd bin
